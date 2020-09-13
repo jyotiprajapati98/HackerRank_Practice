d4bd569e1daa85b122b6db476756518e7b64929d
Jyoti Prajapati <52569075+jyotiprajapati98@users.noreply.github.com>
2020-09-13 15:49:05 +0530

    Create Class-vs-Instance.java

---
+++ 30 Days of Code/Class-vs-Instance.java
@@ -0,0 +1,121 @@
+/**
+Objective
+In this challenge, we're going to learn about the difference between a class and an instance; because this is an Object Oriented concept, it's only enabled in certain languages. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Write a Person class with an instance variable, , and a constructor that takes an integer, , as a parameter. The constructor must assign  to  after confirming the argument passed as  is not negative; if a negative argument is passed as , the constructor should set  to  and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
+
+yearPasses() should increase the  instance variable by .
+amIOld() should perform the following conditional actions:
+If , print You are young..
+If  and , print You are a teenager..
+Otherwise, print You are old..
+Objective
+In this challenge, we're going to learn about the difference between a class and an instance; because this is an Object Oriented concept, it's only enabled in certain languages. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Write a Person class with an instance variable, , and a constructor that takes an integer, , as a parameter. The constructor must assign  to  after confirming the argument passed as  is not negative; if a negative argument is passed as , the constructor should set  to  and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
+
+yearPasses() should increase the  instance variable by .
+amIOld() should perform the following conditional actions:
+If , print You are young..
+If  and , print You are a teenager..
+Otherwise, print You are old..
+
+Complete the method definitions provided in the editor so they meet the specifications outlined above; the code to test your work is already in the editor. If your methods are implemented correctly, each test case will print  or  lines (depending on whether or not a valid  was passed to the constructor).
+
+Sample Input
+
+4
+-1
+10
+16
+18
+Sample Output
+
+Sample Output
+
+Age is not valid, setting age to 0.
+You are young.
+You are young.
+
+You are young.
+You are a teenager.
+
+You are a teenager.
+You are old.
+
+You are old.
+You are old.
+Explanation
+
+Test Case 0: 
+Because , our code must set  to  and print the "Age is not valid..." message followed by the young message. Three years pass and , so we print the young message again.
+
+Test Case 1: 
+Because , our code should print that the person is young. Three years pass and , so we print that the person is now a teenager.
+
+Test Case 2: 
+Because , our code should print that the person is a teenager. Three years pass and , so we print that the person is old.
+
+Test Case 3: 
+Because , our code should print that the person is old. Three years pass and the person is still old at , so we print the old message again.
+
+The extra line at the end of the output is supposed to be there and is trimmed before being compared against the test case's expected output. If you're failing this challenge, check your logic and review your print statements for spelling errors.
+
+*/
+
+//code
+import java.io.*;
+import java.util.*;
+
+public class Person {
+    private int age;	
+  
+	public Person(int initialAge) {
+  		// Add some more code to run some checks on initialAge
+          this.age=initialAge;
+	}
+
+	public void amIOld() {
+  		// Write code determining if this person's age is old and print the correct statement:
+
+          if(age<=0){
+            //if age is negative then write age =0
+            age=0;
+            System.out.println("Age is not valid, setting age to 0.");
+            System.out.println("You are young.");
+          }
+          else if (age<13){
+              System.out.println("You are young.");
+          }
+          else if (age>=13 && age<18){
+              System.out.println("You are a teenager.");
+          }
+          else{
+              System.out.println("You are old.");
+          }
+        
+	}
+
+	public void yearPasses() {
+  		// Increment this person's age.
+          age = age+1;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			int age = sc.nextInt();
+			Person p = new Person(age);
+			p.amIOld();
+			for (int j = 0; j < 3; j++) {
+				p.yearPasses();
+			}
+			p.amIOld();
+			System.out.println();
+        }
+		sc.close();
+    }
+}
